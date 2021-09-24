d071a73a55164416896447fb1324ce4073a1b64c
shirleyzhang2 <40873474+shirleyzhang2@users.noreply.github.com>
2021-09-24 09:34:17 -0400

    Activity 3

--- example.py
+++ example.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+from datetime import datetime
+
 app = Flask(__name__)
+
+moment = Moment(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
-	return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
+
+@app.errorhandler(404)
+
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
