d6a0cae68aa91283984d8eecfc4546cdd9d9db7b
shirleyzhang2 <40873474+shirleyzhang2@users.noreply.github.com>
2021-09-24 12:55:05 -0400

    Activity 4 part 2

--- example.py
+++ example.py
@@ -11,8 +11,13 @@ app.config['SECRET_KEY'] = 'secret key'
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 
+def isEmail(form, field):
+        if '@' not in field.data:
+                raise ValidationError("Please include an '@' in the email address. '%s' is missing an '@'." %(field.data))
+
 class NameForm(Form):
 	name = StringField('What is your name?', validators=[Required()])
+	email = StringField('What is your UofT Email address?', validators=[Required(), isEmail])
 	submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -20,11 +25,15 @@ def index():
 	form = NameForm()
 	if form.validate_on_submit():
 		old_name = session.get('name')
+		old_email = session.get('email')
 		if old_name is not None and old_name != form.name.data:
 			flash('Looks like you have changed your name!')
+		if old_email is not None and old_email != form.email.data:
+                        flash('Looks like you have changed your email!')
 		session['name'] = form.name.data
+		session['email'] = form.email.data
 		return redirect(url_for('index'))
-	return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
+	return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'), email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
